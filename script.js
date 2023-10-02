322dceea5595b4acf9c0764774e3792351d46dd5
Adrian <adrianbyers@gmail.com>
2023-10-02 17:12:26 -0400

    Add dataStorage function and cleanup code

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
+let displayValue = document.querySelector("#display");
+
+const buttons = document.querySelectorAll('.button');
+
 let num1 = 0;
 let operator = "";
 let num2 = 0;
 
-let displayValue = document.querySelector("#display");
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    displayUpdate(button);
+  })
+})
+
+function displayUpdate(button) {
+  if (button.id === "clear") {
+    num1 = 0;
+    operator = "";
+    num2 = 0;
+    return displayValue.textContent = "0";
+  } else if (button.id === "=") {
+    num1 = operate(Number(num1), operator, Number(num2));
+    operator = "";
+    num2 = 0;
+    return displayValue.textContent = num1;
+  } else {
+    if (typeof dataStorage(button) === 'undefined') {
+    return displayValue.textContent === "0" && operator === "" ? displayValue.textContent = button.id : displayValue.textContent += button.id;
+    }
+  }
+}
+
+function dataStorage(button) {
+  if (button.className === "button operator") {
+    if (operator != "") {
+      num1 = operate(Number(num1), operator, Number(num2));
+      operator = button.id;
+      num2 = 0
+      return displayValue.textContent = num1+operator;
+    } else {
+      operator = button.id;
+    }
+  } else {
+    console.log("check")
+    operator === "" ? num1 += button.id : num2 += button.id;
+  }
+  console.log("num1" + num1);
+  console.log("operator" + operator);
+  console.log("num2" + num2);
+}
 
 function operate(num1, operator, num2) {
   return operator === "+" ? add(num1, num2)
     : operator === "-" ? subtract(num1, num2)
     : operator === "*" ? multiply(num1, num2)
     : operator === "/" ? divide(num1, num2)
-    : "Please enter a valid operator";
+    : "Please enter an operator";
 }
 
 function add(num1, num2) {
@@ -27,39 +72,3 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return (num2 === 0) ? "You're trying to divide by zero, not cool!": Math.round((num1 / num2) * 100) / 100;
 }
-
-function displayUpdate(button) {
-  if (button.id === "clear") {
-    num1 = 0;
-    operator = "";
-    num2 =0;
-    return displayValue.textContent = "0";
-  } else if (button.id === "=") {
-    num1 = operate(Number(num1), operator, Number(num2));
-    operator = "";
-    return displayValue.textContent = num1;
-  } else {
-    //if button is a number and operator has no value, add to num1, else add to num2
-    //if button is an operator and operator has an existing value, run operate, else set operator equal to button id
-    if (button.class === "operator") {
-      if (operator != "") {
-        num1 = operate(Number(num1), operator, Number(num2));
-        operator = button.id;
-        return displayValue.textContent = num1+operator;
-      } else {
-        operator = button.id;
-      }
-    } else {
-      operator === "" ? num1 += button.id : num2 += button.id;
-    }
-    return displayValue.textContent === "0" ? displayValue.textContent = button.id : displayValue.textContent += button.id;
-  }
-}
-
-const buttons = document.querySelectorAll('.button')
-
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    displayUpdate(button);
-  })
-})
